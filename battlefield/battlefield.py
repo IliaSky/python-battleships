255f34bed30ff6ef6fb6c49fb673704e75d4ba2b
IliaSky <iliaskywalker@gmail.com>
2013-06-28 03:08:42 +0300

    Drastically simplify battlefield.py

--- battlefield/battlefield.py
+++ battlefield/battlefield.py
@@ -1,25 +1,14 @@
 class Battlefield():
 
     def __init__(self, size, teams):
-        """ Initializes the battlefield, where:
-            + size is the size of a player's side (x, y)
-              - the actual battlefield will have twice those ammounts """
         self.size = size
-        size *= 2
-        self.matrix = [[Cell() for x in range(size.x)] for y in range(size.y)]
+        width, height = 2 * size.x, 2 * size.y
+        self.matrix = [[Cell() for x in range(width)] for y in range(height)]
 
-    @translate_key(self.size)
     def __getitem__(self, key):
+        key += self.size
         return self.matrix[key.y][key.x]
 
-    @translate_key(self.size)
     def __setitem__(self, key, value):
+        key += self.size
         self.matrix[key.y][key.x].contents = value
-
-    def translate_key(offset):
-        def wrap(f):
-            def translated(*args):
-                args.key += offset
-                return f(args)
-            return translated
-        return wrap
--- player.py
+++ player.py
@@ -1,4 +1,6 @@
 class Player:
 
     def __init__(self, battlefield_position):
+        if battlefield_position not in range(4):
+            raise InvalidPositionError
         self.position = battlefield_position
